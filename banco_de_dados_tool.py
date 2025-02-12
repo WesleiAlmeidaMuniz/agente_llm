5e808b4a8570fef28cd8be27c59a75fa46d4f0fa
Weslei Almeida Muniz <88255036+WesleiAlmeidaMuniz@users.noreply.github.com>
2025-02-12 16:53:14 -0300

    feat: Criação de banco e alterações mySQL

--- banco_de_dados_tool.py
+++ banco_de_dados_tool.py
@@ -1,97 +1,80 @@
 # banco_de_dados_tool.py
 from langchain.agents import tool
-import psycopg2
 import os
 import json
 from datetime import datetime
+import mysql.connector
 
-#Busca todos os pedidos com query
-@tool
-def busca_todos_os_pedidos_banco(query: str) -> list:
-    """
-    Busca todos os pedidos no banco de dados com base no status fornecido.
-    
-    Args:
-        status (str): O status dos pedidos a serem buscados no banco de dados.
-        
-    Returns:
-        list: Lista de resultados com os pedidos que correspondem ao status.
-    """ 
-    if "em_processamento" in query.lower():
-        status_pedido = "em_processamento"
-    elif "concluido" in query.lower():
-        status_pedido = "concluido"
-
-    try:
-        conn = psycopg2.connect(
-            host=os.getenv("DB_HOST"),
-            port=os.getenv("DB_PORT"),
-            user=os.getenv("DB_USERNAME"),
-            password=os.getenv("DB_PASSWORD"),
-            database=os.getenv("DB_NAME")
-        )
-        cursor = conn.cursor()
-
-        cursor.execute("SELECT * FROM public.pedido WHERE status = %s", (status_pedido,))
-        resultados = cursor.fetchall()
-
-        resultado_lista = []
-
-        for linha in resultados:
-            dicionario = {
-                "id": linha[0],
-                "valor_total": linha[1],
-                "status": linha[2],
-                 "criado_em": linha[3].strftime("%Y-%m-%d %H:%M:%S") if isinstance(linha[3], datetime) else linha[3],
-                "atualizado_em": linha[4].strftime("%Y-%m-%d %H:%M:%S") if isinstance(linha[4], datetime) else linha[4],
-                "usuarioid": linha[6]
-            }
-
-            resultado_lista.append(dicionario)
-
-        caminho = os.path.join('dadosPedidos', 'resultado.json')
-        with open(caminho, 'w', encoding='utf-8') as f:
-            json.dump(resultado_lista, f, ensure_ascii=False, indent=4)
-
-        cursor.close()
-        conn.close()
-
-        return resultado_lista  # Lista dos pedidos que estão no status 'em_processamento'
-
-    except Exception as e:
-        return []  # Caso ocorra um erro, retorne uma lista vazia
 
 # Função para buscar pedidos no banco de dados
 @tool
-def busca_todos_os_pedidos_com_data(query: str) -> list:
+def busca_todas_as_coletas_data(query: str) -> list:
     """
-    Busca todos os pedidos no banco de dados com base no status fornecido.
+    Busca todas as coletas no banco de dados com base na data fornecida e salva no banco de dados.
     
     Args:
-        status (str): O status dos pedidos a serem buscados no banco de dados.
         date (datetime): Data para filtrar os pedidos.
         
     Returns:
         list: Lista de resultados com os pedidos que correspondem ao status.
     """ 
 
-    input_parts = query.split(", ")
-    status = input_parts[0].split("=")[1].replace('"', '')
-    date_str = input_parts[1].split("=")[1]
+    # date_str = query.split("=")[1].strip().replace('"', '')
 
-    if "em_processamento" in status.lower():
-        status_pedido = "em_processamento"
-    elif "concluido" in status.lower():
-        status_pedido = "concluido"
-
-    date_obj = datetime.strptime(date_str, "%Y-%m-%d")
+    # date_obj = datetime.strptime(date_str, "%Y-%m-%d")
 
     try:
-        # Converte a data para o formato "date" sem considerar a hora
-        date = date_obj.date()
-
         # Estabelecendo a conexão com o banco
-        conn = psycopg2.connect(
+        conn = mysql.connector.connect(
+            host=os.getenv("DB_HOST_TMS"),
+            port=os.getenv("DB_PORT_TMS"),
+            user=os.getenv("DB_USERNAME_TMS"),
+            password=os.getenv("DB_PASSWORD_TMS"),
+            database=os.getenv("DB_NAME_TMS")
+        )
+        cursor = conn.cursor(dictionary=True)
+
+        # Executa a consulta SQL com comparação apenas de data
+        cursor.execute("""
+                       SELECT 
+                            ef.id AS evento_id,
+                            mt.id AS minuta, 
+                            mt.codigo_rastreio AS codigo_rastreio,
+                            mt.created_at AS criado_em,
+                            ct.chave_nf as chave_nf,
+                            et.nome as nome_cliente,
+                            edr.logradouro as rua,
+                            edr.numero as numero_rua,
+                            bir.nome as bairro,
+                            cdd.nome as cidade,
+                            ufs.nome as estado,
+                            edr.complemento as complemento
+                        FROM ccdblog_eventos.frete_criado ef
+                        INNER JOIN ccdblog_temis.minutas mt ON ef.minutas_id = mt.id
+                        LEFT JOIN ccdblog_temis.coletas ct on mt.id = ct.minuta_id
+                        LEFT JOIN ccdblog_temis.entidades et on et.id = ct.remetente
+                        LEFT JOIN ccdblog_temis.enderecos edr on edr.entidade_id = et.id
+                        LEFT JOIN ccdblog_temis.bairros bir on bir.id = edr.bairro_id
+                        LEFT JOIN ccdblog_temis.cidades cdd on bir.cidade_id = cdd.id
+                        LEFT JOIN ccdblog_temis.ufs ufs on cdd.uf_id = ufs.id
+                        where DATE(mt.created_at) = %s;""", (query,))
+        resultados = cursor.fetchall()
+
+        salvar_no_banco(resultados)
+
+        cursor.close()
+        conn.close()
+
+        return resultados  # Lista dos pedidos que estão no status 'em_processamento'
+
+    except Exception as e:
+        print(f"Erro ao buscar pedidos: {e}")
+        return []  # Caso ocorra um erro, retorne uma lista vazia
+    
+
+def salvar_no_banco(resultados: list):
+    try:
+        conn = mysql.connector.connect(
             host=os.getenv("DB_HOST"),
             port=os.getenv("DB_PORT"),
             user=os.getenv("DB_USERNAME"),
@@ -100,38 +83,30 @@ def busca_todos_os_pedidos_com_data(query: str) -> list:
         )
         cursor = conn.cursor()
 
-        # Executa a consulta SQL com comparação apenas de data
-        cursor.execute("SELECT * FROM public.pedido WHERE status = %s and DATE(created_at) = %s", (status_pedido, date))
-        resultados = cursor.fetchall()
-
-        resultado_lista = []
-
         for linha in resultados:
-            # Cria um dicionário com os resultados formatados
-            dicionario = {
-                "id": linha[0],
-                "valor_total": linha[1],
-                "status": linha[2],
-                "criado_em": linha[3].strftime("%Y-%m-%d %H:%M:%S") if isinstance(linha[3], datetime) else linha[3],
-                "atualizado_em": linha[4].strftime("%Y-%m-%d %H:%M:%S") if isinstance(linha[4], datetime) else linha[4],
-                "usuarioid": linha[6]
-            }
+            # Verifica se a minuta já existe na tabela
+            cursor.execute("SELECT 1 FROM verificados WHERE minuta = %s LIMIT 1", (linha["minuta"],))
+            existe = cursor.fetchone()
 
-            resultado_lista.append(dicionario)
+            if not existe:  # Se não existir, insere no banco
+                cursor.execute("""
+                    INSERT INTO verificados (
+                        minuta, chave_nf, dt_nf, valor_nf, peso_bruto, peso_liquido, 
+                        peso_cubado, volumes, status_coleta, criado_em, atualizado_em, 
+                        codigo_rastreio, status_minuta, tipo, nome_embarcador, 
+                        documento_embarcador, status_entidade, tipo_entidade
+                    ) 
+                    VALUES (
+                        %(minuta)s, %(chave_nf)s, %(dt_nf)s, %(valor_nf)s, %(peso_bruto)s, %(peso_liquido)s, 
+                        %(peso_cubado)s, %(volumes)s, %(status_coleta)s, %(criado_em)s, %(atualizado_em)s, 
+                        %(codigo_rastreio)s, %(status_minuta)s, %(tipo)s, %(nome_embarcador)s, 
+                        %(documento_embarcador)s, %(status_entidade)s, %(tipo_entidade)s
+                    );
+                """, linha)
 
-        # Salva o resultado em um arquivo JSON
-        data_agora = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-        path = './dadosPedidos'
-        file_name = f'resultado - {status_pedido} - {data_agora}.json'
-        caminho = os.path.join(path, file_name)
-        with open(caminho, 'w', encoding='utf-8') as f:
-            json.dump(resultado_lista, f, ensure_ascii=False, indent=4)
-
+        conn.commit()
         cursor.close()
         conn.close()
 
-        return resultado_lista  # Lista dos pedidos que estão no status 'em_processamento'
-
     except Exception as e:
-        print(f"Erro ao buscar pedidos: {e}")
-        return []  # Caso ocorra um erro, retorne uma lista vazia
+        print(f"Erro ao salvar pedidos: {e}")
--- main.py
+++ main.py
@@ -1,20 +1,12 @@
 from dotenv import load_dotenv
 from langchain import hub
 from langchain_ollama import ChatOllama
-from langchain_community.tools import YouTubeSearchTool
-from langchain_community.tools.google_trends import GoogleTrendsQueryRun
-from langchain_community.utilities.google_trends import GoogleTrendsAPIWrapper
 from langchain.agents import AgentExecutor, create_react_agent
-from banco_de_dados_tool import busca_todos_os_pedidos_com_data, busca_todos_os_pedidos_banco
+from banco_de_dados_tool import busca_todas_as_coletas_data
 
 load_dotenv()
 
-#Tools
-youtube_tool = YouTubeSearchTool()
-google_trends = GoogleTrendsQueryRun(api_wrapper=GoogleTrendsAPIWrapper())
-
-
-tools = [youtube_tool, google_trends, busca_todos_os_pedidos_com_data, busca_todos_os_pedidos_banco]
+tools = [busca_todas_as_coletas_data]
 
 #Prompt
 prompt = hub.pull("hwchase17/react")
@@ -25,15 +17,11 @@ llm = ChatOllama(model='gemma2', temperature=0)
 agent = create_react_agent(llm=llm, tools=tools, prompt=prompt)
 
 # Executor
-agente_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, handle_parsing_errors=True)
+agente_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 
 
 response =agente_executor.invoke({'input': """
-                                  Eu quero processar um fluxo de dados sobre pedidos. Cada pedido tem um id, valor_total, status, criado_em e atualizado_em. 
-                                    O fluxo deve passar por esses estágios:
-                                    1. Buscar no banco de dados todos os pedidos com status "em_processamento".
-                                    2. Fazer a soma de todos os pedidos com a variavel valor_total.
-                                    3. Caso contrário, deve ser ignorado.
+                                  Busque para mim dados de coleta na data = 2025-01-16
                                   """})
 
 print(response['output'])
